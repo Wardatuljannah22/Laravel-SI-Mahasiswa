e347cfd7c7a33e819a56624cb48b4d65774bdc72
WardatulJannah22 <wardatuljannah525@gmail.com>
2020-04-23 08:58:23 +0700

    Datatable_ActionColumn

--- resources/views/biodata/index.blade.php
+++ resources/views/biodata/index.blade.php
@@ -1,42 +1,16 @@
+  
 @extends("layout")
 
+@push("style")
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.css"/>
+@endpush
+
 @section("content")
-<h1>Daftar Mahasiswa</h1>
-<a href="{{ route('biodata.create') }}" class="btn btn-primary">
-<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-create</a></p>
-
-<a href="/biodata-mahasiswa/export_excel" class="btn btn-success my-3" target="_blank">EXPORT EXCEL</a></p>
-
-<table class ="table table-striped  table-hover" id="datatable">
-	<thead>
-		<tr>
-			<th>ID</th>
-			<th>NAMA</th>
-			<th>NIM</th>
-			<th>ACTION</th>
-
-		</tr>
-	</thead>
-	<tbody>
-		@php $i=1 @endphp
-		@forelse($mahasiswa as $data)
-		<tr>
-			<td>{{ $data->id ++}}</td>
-			<td>{{ $data->name }}</td>
-			<td>{{ $data->nim }}</td>
-			<td>
-				<a href="{{ route('biodata.show', ['id' => $data->id]) }}" class="btn btn-success">Detail</a>
-				<a href="{{ route('biodata.edit', ['id' => $data->id]) }}" class="btn btn-warning">Edit</a>
-				<!-- <a href="{{ route('biodata.destroy', ['id' => $data->id]) }}" class="btn btn-danger">Delete</a> -->
-				<a onclick="return confirm('Apakah anda yakin?');" href="{{ route('biodata.destroy', ['id' => $data->id])}}" class="btn btn-danger">Delete</a>
-			</td>
-		</tr>
-		@empty
-		<tr>
-			<td colspan="4">Data belum tersedia!</td>
-		</tr>
-		@endforelse
-	</tbody>
-</table>
+    <h1>Daftar Mahasiswa</h1>
+    {!! $html->table() !!}
 @endsection
+
+@push("script")
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>
+    {!! $html->scripts() !!}
+@endpush
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,8 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-
-    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-
+    
+    @stack("style")
 </head>
 <body>
 
@@ -28,35 +26,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Bootstrap script -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-<script type="text/javascript">
-    
-    $(document).ready(function () {
-
-        var table = $('#datatable').DataTable();
-
-        table.on('click', '.detail', function () {
-
-            $tr = $(this).closest('tr');
-            if ($($tr).hasClass('child')) {
-                $tr = $tr.prev('.parent');
-            }
-
-            var data = table.row($tr).data();
-            console.log(data);
-
-            $('#id').val(data[0]);
-            $('#name').val(data[1]);
-            $('#nim').val(data[2]);
-            
-            $('#EditModal').modal('show');
-
-        })
-
-    })
-
-</script>
+    @stack("script")
 </body>
 </html>
